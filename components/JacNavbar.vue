08ac11b1f8d571064e3631a5597ca1b08b5b66f7
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-06-11 17:02:54 -0400

    navigation bar

---
+++ components/JacNavbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <div class="container">
+            <a class="navbar-brand" href="/">
+                <img src="~assets/images/logo.svg" alt="JAC Network" style="width: 250px;">
+            </a>
+            <ul class="navbar-nav ml-auto d-none d-lg-flex">
+                <li class="nav-item mr-5"><router-link to="/" class="nav-link">Home</router-link></li>
+                <li class="nav-item mr-5"><router-link to="/" class="nav-link">Forums</router-link></li>
+                <li class="nav-item mr-5"><router-link to="/" class="nav-link">Events</router-link></li>
+                <li class="nav-item mr-5"><router-link to="/" class="nav-link">Store</router-link></li>
+                <li class="nav-item mr-5"><router-link to="/" class="nav-link">Votes</router-link></li>
+                <li class="nav-item mr-5"><router-link to="/" class="nav-link">Support</router-link></li>
+                <li class="nav-item mr-5 button"><router-link to="/" class="nav-link"></router-link></li>
+            </ul>
+            <div class="d-lg-none">
+                <button class="hamburger hamburger--slider" type="button" aria-label="Menu" aria-controls="navigation" :class="{ 'is-active': displayMobileMenu }"  @click="toggleHamburger">
+                        <span class="hamburger-box">
+                            <span class="hamburger-inner"></span>
+                        </span>
+                </button>
+            </div>
+        </div>
+        <transition name="fade">
+            <div v-show="displayMobileMenu" class="row d-lg-none mobile-nav mx-auto py-4">
+                <ul class="m-0 p-0 list-unstyled text-center mx-auto">
+
+                </ul>
+            </div>
+        </transition>
+    </nav>
+</template>
+<script>
+    export default {
+        name: 'JacFooter'
+    }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,29 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        jacnetwork
-      </h1>
-      <h2 class="subtitle">
-        My unreal Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="header">
+      <JacNavbar></JacNavbar>
+      <div class="header-inner">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h3 class="fs-36 font-white">Urna morbi dictum sodales enim!</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import JacNavbar from '../components/JacNavbar';
 export default {
-  components: {
-    Logo
-  }
+  layout: 'default',
+  head() {
+    return {
+      title: 'JAC Network - Home'
+    }
+  },
+  components: {JacNavbar}
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
