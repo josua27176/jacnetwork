2167dedad4b7e84e32d8864157e0069fab7cf934
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-06-16 03:34:17 -0400

    finished announcements

--- pages/index.vue
+++ pages/index.vue
@@ -29,29 +29,63 @@
         <!-- Announcements -->
         <div class="col-xl-8">
           <h4 class="text-white fw-700 fs-24 text-uppercase mt-5 mb-4">Announcements</h4>
-          <div class="announcement">
-            <div class="a-header">
-              <img src="~assets/images/a-header-bg.png" class="w-100">
-              <div class="a-info">
-                <h3 class="font-white fs-30 text-center fw-700 text-uppercase mb-0">Announcement Title</h3>
-                <p class="fs-18 font-light lh-2 mb-4 text-center">3 Minute Read</p>
+          <div class="row">
+            <div class="col-md-12 mb-5">
+              <div class="announcement">
+                <div class="a-header">
+                  <img src="~assets/images/a-header-bg.png" class="w-100">
+                  <div class="a-info">
+                    <h3 class="font-white fs-30 text-center fw-700 text-uppercase mb-0">Announcement Title</h3>
+                    <p class="fs-18 font-light lh-2 mb-4 text-center">3 Minute Read</p>
+                  </div>
+                </div>
+                <div class="a-content bg-dark px-5 pb-4 mb-0">
+                  <h4 class="fs-22 text-white fw-700 text-uppercase">Title One</h4>
+                  <p class="fs-18 font-light lh-2 mb-4">Malesuada tincidunt in nunc egestas sed velit. Id et vitae vitae pellentesque dignissim amet enim. Fringilla consequat scelerisque aliquet elit. Augue congue volutpat vitae. Fringilla consequat scelerisque aliquet elit.</p>
+                  <p class="fs-18 font-white lh-2 blockquote mb-4"><span class="d-block ml-4">Dictum ut maecenas orci, morbi. Molestie porttitor turpis sit tortor lacus, purus arcu sed commodo. Et scelerisque a vel, pellentesque et diam nullam. Eget ipsum erat varius amet pellentesque. Orci enim sed arcu vestibulum. </span></p>
+                  <p class="fs-18 font-light lh-2">Malesuada tincidunt in nunc egestas sed velit. Id et vitae vitae pellentesque dignissim amet enim. Fringilla consequat scelerisque aliquet elit. Augue congue volutpat vitae. Fringilla consequat scelerisque aliquet elit.</p>
+                </div>
+                <div class="a-footer px-5 py-3">
+                  <div class="row">
+                    <div class="col-6">
+                      <p class="fs-16 font-white m-0 p-0"><i class="fa fa-user-circle mr-2" aria-hidden="true"></i> Administrator</p>
+                    </div>
+                    <div class="col-6">
+                      <p class="fs-16 font-white m-0 p-0 text-md-right"><i class="fa fa-calendar-o mr-2" aria-hidden="true"></i> May 09, 2020</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="a-content bg-dark px-5 pb-4 mb-0">
-              <h4 class="fs-22 text-white fw-700 text-uppercase">Title One</h4>
-              <p class="fs-18 font-light lh-2 mb-4">Malesuada tincidunt in nunc egestas sed velit. Id et vitae vitae pellentesque dignissim amet enim. Fringilla consequat scelerisque aliquet elit. Augue congue volutpat vitae. Fringilla consequat scelerisque aliquet elit.</p>
-              <p class="fs-18 font-white lh-2 blockquote mb-4"><span class="d-block ml-4">Dictum ut maecenas orci, morbi. Molestie porttitor turpis sit tortor lacus, purus arcu sed commodo. Et scelerisque a vel, pellentesque et diam nullam. Eget ipsum erat varius amet pellentesque. Orci enim sed arcu vestibulum. </span></p>
-              <p class="fs-18 font-light lh-2">Malesuada tincidunt in nunc egestas sed velit. Id et vitae vitae pellentesque dignissim amet enim. Fringilla consequat scelerisque aliquet elit. Augue congue volutpat vitae. Fringilla consequat scelerisque aliquet elit.</p>
-            </div>
-            <div class="a-footer px-5 py-3">
-              <div class="row">
-                <div class="col-md-8">
-                  <p class="fs-16 font-white m-0 p-0"><i class="fa fa-user-circle mr-2" aria-hidden="true"></i> Administrator</p>
+            <div class="col-md-12 mb-5">
+              <div class="announcement">
+                <div class="a-header">
+                  <img src="~assets/images/a-header-bg.png" class="w-100">
+                  <div class="a-info">
+                    <h3 class="font-white fs-30 text-center fw-700 text-uppercase mb-0">Announcement Title</h3>
+                    <p class="fs-18 font-light lh-2 mb-4 text-center">3 Minute Read</p>
+                  </div>
                 </div>
-                <div class="col-md-4">
-                  <p class="fs-16 font-white m-0 p-0 text-md-right"><i class="fa fa-calendar-o mr-2" aria-hidden="true"></i> May 09, 2020</p>
+                <div class="a-content bg-dark px-5 pb-4 mb-0">
+                  <h4 class="fs-22 text-white fw-700 text-uppercase">Title One</h4>
+                  <p class="fs-18 font-light lh-2 mb-4">Malesuada tincidunt in nunc egestas sed velit. Id et vitae vitae pellentesque dignissim amet enim. Fringilla consequat scelerisque aliquet elit. Augue congue volutpat vitae. Fringilla consequat scelerisque aliquet elit.</p>
+                  <p class="fs-18 font-white lh-2 blockquote mb-4"><span class="d-block ml-4">Dictum ut maecenas orci, morbi. Molestie porttitor turpis sit tortor lacus, purus arcu sed commodo. Et scelerisque a vel, pellentesque et diam nullam. Eget ipsum erat varius amet pellentesque. Orci enim sed arcu vestibulum. </span></p>
+                  <p class="fs-18 font-light lh-2">Malesuada tincidunt in nunc egestas sed velit. Id et vitae vitae pellentesque dignissim amet enim. Fringilla consequat scelerisque aliquet elit. Augue congue volutpat vitae. Fringilla consequat scelerisque aliquet elit.</p>
+                </div>
+                <div class="a-footer px-5 py-3">
+                  <div class="row">
+                    <div class="col-6">
+                      <p class="fs-16 font-white m-0 p-0"><i class="fa fa-user-circle mr-2" aria-hidden="true"></i> Administrator</p>
+                    </div>
+                    <div class="col-6">
+                      <p class="fs-16 font-white m-0 p-0 text-md-right"><i class="fa fa-calendar-o mr-2" aria-hidden="true"></i> May 09, 2020</p>
+                    </div>
+                  </div>
                 </div>
               </div>
+            </div>
+            <div class="col-md-12 mb-5">
+              <a class="d-block text-center text-uppercase fs-22 text-white fw-600 f-clarika">Load More</a>
             </div>
           </div>
         </div>
