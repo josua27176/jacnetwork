a8a7325deb94f258e773aa5f5897efd0868013d1
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-06-16 23:27:54 -0400

    finished server list page

--- pages/servers.vue
+++ pages/servers.vue
@@ -11,10 +11,10 @@
                 </div>
             </div>
         </div>
-        <div class="servers">
+        <div class="servers mb-5">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-9">
+                    <div class="col-lg-9 mb-4">
                         <div class="bg-dark rounded px-4 py-4">
                             <div class="row">
                                 <div class="col-lg-5">
@@ -50,24 +50,201 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-lg-3 mb-4">
                         <div class="bg-dark rounded px-4 py-4 h-100">
                             <div class="row">
-                                <div class="col-md-10">
+                                <div class="col-10">
                                     <h4 class="fs-20 font-green fw-600">3/30 Online Players</h4>
                                 </div>
-                                <div class="col-md-2">
+                                <div class="col-2">
                                     <a href="#" class="info">
-                                        <i class="fa fa-info-circle fs-24" aria-hidden="true"></i>
+                                        <i class="fa fa-info-circle fs-30" aria-hidden="true"></i>
                                     </a>
                                 </div>
-                                <div class="col-md-3 mt-3">
+                                <div class="col-3 mt-3">
                                     <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
                                 </div>
-                                <div class="col-md-3 mt-3">
+                                <div class="col-3 mt-3">
                                     <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
                                 </div>
-                                <div class="col-md-3 mt-3">
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 mb-4">
+                        <div class="bg-dark rounded px-4 py-4">
+                            <div class="row">
+                                <div class="col-lg-5">
+                                    <img src="https://placehold.it/320x230" class="rounded w-100 mb-4 mb-xl-0">
+                                </div>
+                                <div class="col-xl-7">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h3 class="text-white fs-24 fw-600 text-uppercase">Just Enough Stuff</h3>
+                                            <p class="font-grey fs-14">Modpack | Minecraft 1.15.2</p>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <span class="bg-green status w-100 d-block mt-2">Online</span>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <p class="text-white fs-18 lh-2">Aliquet venenatis, amet morbi pulvinar auctor molestie proin. Id dictum vulputate faucibus metus dolor id odio ac. </p>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="button-sm-round float-left mr-4" >
+                                                <a href="" class="float-left" style="width: 300px;">
+                                                    jes.jac.network
+                                                    <span class="ml-3 bg-black d-inline-block status" style="width: 80px;">Copy</span>
+                                                </a>
+                                            </div>
+                                            <div class="button-sm-round" style="width: 180px;">
+                                                <a href="" class="float-left w-100">
+                                                    Server Events
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-4">
+                        <div class="bg-dark rounded px-4 py-4 h-100">
+                            <div class="row">
+                                <div class="col-10">
+                                    <h4 class="fs-20 font-green fw-600">3/30 Online Players</h4>
+                                </div>
+                                <div class="col-2">
+                                    <a href="#" class="info">
+                                        <i class="fa fa-info-circle fs-30" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 mb-4">
+                        <div class="bg-dark rounded px-4 py-4">
+                            <div class="row">
+                                <div class="col-lg-5">
+                                    <img src="https://placehold.it/320x230" class="rounded w-100 mb-4 mb-xl-0">
+                                </div>
+                                <div class="col-xl-7">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h3 class="text-white fs-24 fw-600 text-uppercase">Just Enough Stuff</h3>
+                                            <p class="font-grey fs-14">Modpack | Minecraft 1.15.2</p>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <span class="bg-green status w-100 d-block mt-2">Online</span>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <p class="text-white fs-18 lh-2">Aliquet venenatis, amet morbi pulvinar auctor molestie proin. Id dictum vulputate faucibus metus dolor id odio ac. </p>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="button-sm-round float-left mr-4" >
+                                                <a href="" class="float-left" style="width: 300px;">
+                                                    jes.jac.network
+                                                    <span class="ml-3 bg-black d-inline-block status" style="width: 80px;">Copy</span>
+                                                </a>
+                                            </div>
+                                            <div class="button-sm-round" style="width: 180px;">
+                                                <a href="" class="float-left w-100">
+                                                    Server Events
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-4">
+                        <div class="bg-dark rounded px-4 py-4 h-100">
+                            <div class="row">
+                                <div class="col-10">
+                                    <h4 class="fs-20 font-green fw-600">3/30 Online Players</h4>
+                                </div>
+                                <div class="col-2">
+                                    <a href="#" class="info">
+                                        <i class="fa fa-info-circle fs-30" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 mb-4">
+                        <div class="bg-dark rounded px-4 py-4">
+                            <div class="row">
+                                <div class="col-lg-5">
+                                    <img src="https://placehold.it/320x230" class="rounded w-100 mb-4 mb-xl-0">
+                                </div>
+                                <div class="col-xl-7">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h3 class="text-white fs-24 fw-600 text-uppercase">Just Enough Stuff</h3>
+                                            <p class="font-grey fs-14">Modpack | Minecraft 1.15.2</p>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <span class="bg-green status w-100 d-block mt-2">Online</span>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <p class="text-white fs-18 lh-2">Aliquet venenatis, amet morbi pulvinar auctor molestie proin. Id dictum vulputate faucibus metus dolor id odio ac. </p>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="button-sm-round float-left mr-4" >
+                                                <a href="" class="float-left" style="width: 300px;">
+                                                    jes.jac.network
+                                                    <span class="ml-3 bg-black d-inline-block status" style="width: 80px;">Copy</span>
+                                                </a>
+                                            </div>
+                                            <div class="button-sm-round" style="width: 180px;">
+                                                <a href="" class="float-left w-100">
+                                                    Server Events
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-4">
+                        <div class="bg-dark rounded px-4 py-4 h-100">
+                            <div class="row">
+                                <div class="col-10">
+                                    <h4 class="fs-20 font-green fw-600">3/30 Online Players</h4>
+                                </div>
+                                <div class="col-2">
+                                    <a href="#" class="info">
+                                        <i class="fa fa-info-circle fs-30" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                                <div class="col-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                                <div class="col-3 mt-3">
                                     <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
                                 </div>
                             </div>
