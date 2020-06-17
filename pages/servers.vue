59d99e0f9321595efaa12d87be3102f5558f07e5
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-06-16 23:19:27 -0400

    server list item

---
+++ pages/servers.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <div class="header-alt">
+            <JacNavbar></JacNavbar>
+            <div class="header-inner">
+                <div class="container">
+                    <div class="col-xl-8 mx-auto">
+                        <h2 class="text-white fw-600 text-center fs-48">Server List</h2>
+                        <p class="font-light fs-20 text-center lh-2">Malesuada tincidunt in nunc egestas sed velit. Id et vitae vitae pellentesque dignissim amet enim. Fringilla consequat scelerisque aliquet elit. Augue congue volutpat vitae.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="servers">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="bg-dark rounded px-4 py-4">
+                            <div class="row">
+                                <div class="col-lg-5">
+                                    <img src="https://placehold.it/320x230" class="rounded w-100 mb-4 mb-xl-0">
+                                </div>
+                                <div class="col-xl-7">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h3 class="text-white fs-24 fw-600 text-uppercase">Just Enough Stuff</h3>
+                                            <p class="font-grey fs-14">Modpack | Minecraft 1.15.2</p>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <span class="bg-green status w-100 d-block mt-2">Online</span>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <p class="text-white fs-18 lh-2">Aliquet venenatis, amet morbi pulvinar auctor molestie proin. Id dictum vulputate faucibus metus dolor id odio ac. </p>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="button-sm-round float-left mr-4" >
+                                                <a href="" class="float-left" style="width: 300px;">
+                                                    jes.jac.network
+                                                    <span class="ml-3 bg-black d-inline-block status" style="width: 80px;">Copy</span>
+                                                </a>
+                                            </div>
+                                            <div class="button-sm-round" style="width: 180px;">
+                                                <a href="" class="float-left w-100">
+                                                    Server Events
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="bg-dark rounded px-4 py-4 h-100">
+                            <div class="row">
+                                <div class="col-md-10">
+                                    <h4 class="fs-20 font-green fw-600">3/30 Online Players</h4>
+                                </div>
+                                <div class="col-md-2">
+                                    <a href="#" class="info">
+                                        <i class="fa fa-info-circle fs-24" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                                <div class="col-md-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                                <div class="col-md-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                                <div class="col-md-3 mt-3">
+                                    <img src="https://crafatar.com/renders/head/a24092c9-af15-4798-aac8-df41ea1fec94.png" class="w-100" alt="Rowzy">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import JacNavbar from '../components/JacNavbar';
+    import JacSlider from '../components/JacSlider';
+
+    export default {
+        layout: 'default',
+        name: 'Home',
+        head() {
+            return {
+                title: 'JAC Network - Server List'
+            }
+        },
+        components: {JacNavbar, JacSlider}
+    }
+</script>
