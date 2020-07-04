4fd5a6e71ec314a5e3066eea22f4c1899187f945
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-07-04 03:41:53 -0400

    timestamp format

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -3,8 +3,9 @@
         <JacAdminNav></JacAdminNav>
         <div class="container mb-5">
             <h2 class="fs-36 font-white fw-700 mb-4">Announcements</h2>
-            <div class="col-md-12 bg-dark rounded py-5 px-5" v-for="post in posts">
-                <h4 class="fs-22 text-white fw-600 mt-0 mb-3"> {{ post.title }}</h4>
+            <div class="col-md-12 bg-dark rounded py-5 px-5 mb-5" v-for="post in posts">
+                <h2 class="fs-22 text-white fw-600 mt-0 mb-2"> {{ post.title }}</h2>
+                <h4 class="fs-18 text-white mb-4">{{ post.timestamp | moment().format("MMM DD, YYYY") }}</h4>
                 <p class="fs-18 text-white lh-25 mb-4">{{ post.body }}</p>
                 <div class="button-group d-flex">
                     <div class="button-outline float-md-left">
