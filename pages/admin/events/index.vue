0a54b7e75871c92f04af85d6a9049764e0dfcbd6
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-07-04 03:18:12 -0400

    dynamic event page

--- pages/admin/events/index.vue
+++ pages/admin/events/index.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
         <JacAdminNav></JacAdminNav>
-        <div class="container">
-            <h2 class="fs-36 font-white fw-700 mb-4">Events</h2>
+        <div class="container mb-5">
+
             <div class="row">
-                <div class="col-md-3" v-for="event in events">
-                    <h4 class="fs-22 text-white fw-600 mb-3">{{ event.title }}</h4>
-                    <h4 class="fs-14 text-white fw-600 mb-3">{{ event.start }}</h4>
-                    <p class="fs-16 text-white fw-400 lh-25">{{ event.body }}</p>
-                    <div class="button-blue">
+                <div class="col-md-10">
+                    <h2 class="fs-36 font-white fw-700 mb-4">Events</h2>
+                </div>
+                <div class="col-md-2">
+                    <div class="button-blue mt-2 ml-auto">
+                        <NuxtLink to="/admin/events/new">New</NuxtLink>
+                    </div>
+                </div>
+                <div class="col-md-3 bg-dark rounded py-4" v-for="event in events">
+                    <h4 class="fs-16 text-white fw-400 mt-0 mb-4 text-center"> {{ event.start | moment().format("MM-DD-YYYY") }}</h4>
+                    <h4 class="fs-22 text-white fw-600 mb-4 text-center">{{ event.title }}</h4>
+                    <div class="button-blue mx-auto">
                         <a href="#">Edit</a>
                     </div>
                 </div>
