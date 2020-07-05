5ad17162924b59056e7c935977b7e0579a9a17b4
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-07-05 01:43:58 -0400

    all events spacing

--- pages/admin/events/index.vue
+++ pages/admin/events/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <JacAdminNav></JacAdminNav>
         <div class="container mb-5">
 
             <div class="row">
@@ -12,11 +11,13 @@
                         <NuxtLink to="/admin/events/new">New</NuxtLink>
                     </div>
                 </div>
-                <div class="col-md-3 bg-dark rounded py-4" v-for="event in events">
-                    <h4 class="fs-16 text-white fw-400 mt-0 mb-4 text-center"> {{ event.start | moment().format("MM-DD-YYYY") }}</h4>
-                    <h4 class="fs-22 text-white fw-600 mb-4 text-center">{{ event.title }}</h4>
-                    <div class="button-blue mx-auto">
-                        <a href="#">Edit</a>
+                <div class="col-md-3 mb-4" v-for="event in events">
+                    <div class="bg-dark rounded py-4">
+                        <h4 class="fs-18 text-white fw-400 mt-0 mb-4 text-center"> {{ event.start | moment().format("MMM DD YYYY") }}</h4>
+                        <h4 class="fs-22 text-white fw-600 mb-4 text-center">{{ event.title }}</h4>
+                        <div class="button-blue mx-auto">
+                            <nuxt-link :to="`events/${event.id}`">test</nuxt-link>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -25,17 +26,17 @@
 </template>
 <script>
     import { mapState, mapActions, mapGetters } from 'vuex';
-    import JacAdminNav from "@/components/JacAdminNav";
 
     export default {
         middleware: 'auth',
-        components: {JacAdminNav},
+        layout: 'admin',
         data() {
             return {
                 events: []
             }
         },
         created() {
+
             this.$axios
                 .$get(`/events`)
                 .then((res) => {
