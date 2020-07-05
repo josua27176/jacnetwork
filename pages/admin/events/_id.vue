377212bfb57b42f6652b3e7db5d1f9ccbf3f1c6a
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-07-05 01:46:29 -0400

    individual event page

---
+++ pages/admin/events/_id.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="container">
+            <h2 class="fs-36 font-white fw-700 mb-4">Edit Event</h2>
+            <form @submit.prevent="">
+                <div class="row">
+                    <div class="col-md-12">
+                        <label class="mb-3">Event Title:</label>
+                        <input type="text" name="event_name" class="mb-4"  :value="`${this.eventTitle}`">
+                    </div>
+                    <div class="col-md-12">
+                        <label class="mb-3">Event Description:</label>
+                        <textarea rows="5" class="mb-5" :value="`${this.eventBody}`"></textarea>
+                    </div>
+                    <div class="col-md-5">
+                        <button type="submit" class="form-button bg-blue mr-4">Save</button>
+                        <button @click="deleteEvent" type="submit" class="form-button bg-dark">Delete</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        middleware: 'auth',
+        layout: 'admin',
+        data() {
+          return {
+              eventTitle: '',
+              eventBody: '',
+          }
+        },
+        methods: {
+            updateEvent() {
+
+            },
+            async deleteEvent() {
+                let token = this.$auth.getToken('local');
+                this.$axios.$delete('/events/' + this.$route.params.id,
+                        {
+                            headers: {
+                                Authorization: token
+                            },
+                        },
+                    )
+                    .then((res) => {
+                        this.$toast.success('Event has been deleted.')
+                        this.$router.push('/admin/events');
+                    })
+            }
+        },
+        created() {
+            this.$axios
+                .$get('/events/' + this.$route.params.id)
+                .then((res) => {
+                    this.eventTitle = res.title;
+                    this.eventBody = res.body;
+                })
+        }
+    }
+</script>
