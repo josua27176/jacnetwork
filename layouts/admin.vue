f382e545de97cdd79a3bb14bcd58a5a8d80dd0c9
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-07-05 01:46:16 -0400

    admin page layout

---
+++ layouts/admin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <JacAdminNav></JacAdminNav>
+        <nuxt />
+        <JacFooter></JacFooter>
+    </div>
+</template>
+<script>
+    import JacAdminNav from "@/components/JacAdminNav";
+    import JacFooter from '../components/JacFooter';
+    export default  {
+        components: {JacAdminNav, JacFooter}
+    }
+</script>
+
