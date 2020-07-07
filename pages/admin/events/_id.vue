2e5ea2c8d4953653339fd097a5f02f8645a23942
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-07-07 01:06:51 -0400

    placeholder for registered users

--- pages/admin/events/_id.vue
+++ pages/admin/events/_id.vue
@@ -1,22 +1,30 @@
 <template>
     <div class="container mb-5">
-        <h2 class="fs-36 font-white fw-700 mb-4">Edit Event</h2>
-        <form @submit.prevent="">
-            <div class="row">
-                <div class="col-md-12">
-                    <label class="mb-3">Event Title:</label>
-                    <input type="text" name="event_name" class="mb-4"  :value="`${this.eventTitle}`">
-                </div>
-                <div class="col-md-12">
-                    <label class="mb-3">Event Description:</label>
-                    <textarea rows="5" class="mb-5" :value="`${this.eventBody}`"></textarea>
-                </div>
-                <div class="col-md-5">
-                    <button type="submit" class="form-button bg-blue mr-4">Save</button>
-                    <button @click="deleteEvent" type="submit" class="form-button bg-dark">Delete</button>
-                </div>
+
+        <div class="row">
+            <div class="col-md-8">
+                <h2 class="fs-36 font-white fw-700 mb-4">Edit Event</h2>
+                <form @submit.prevent="">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label class="mb-3">Event Title:</label>
+                            <input type="text" name="event_name" class="mb-4"  :value="`${this.eventTitle}`">
+                        </div>
+                        <div class="col-md-12">
+                            <label class="mb-3">Event Description:</label>
+                            <textarea rows="5" class="mb-5" :value="`${this.eventBody}`"></textarea>
+                        </div>
+                        <div class="col-md-5">
+                            <button type="submit" class="form-button bg-blue mr-4">Save</button>
+                            <button @click="deleteEvent" type="submit" class="form-button bg-dark">Delete</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </form>
+            <div class="col-md-4">
+                <h2 class="fs-36 font-white fw-700 mb-4">Registered</h2>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -54,6 +62,11 @@
                 .then((res) => {
                     this.eventTitle = res.title;
                     this.eventBody = res.body;
+                })
+            this.$axios
+                .$get('/events/' + this.$route.params.id + '/participants')
+                .then((res) => {
+                    console.log(res)
                 })
         }
     }
