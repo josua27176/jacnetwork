49c29200bde3ed0620d7c01e68a90f301c2b5476
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-07-07 02:25:44 -0400

    swiper intergration

--- components/JacEvents.vue
+++ components/JacEvents.vue
@@ -1,6 +1,6 @@
 <template>
-    <swiper ref="JacEvents">
-        <swiper-slide>
+    <swiper ref="JacEvents" :options="swiperOptions">
+        <swiper-slide v-for="event in events" :key="event.id">
             <div class="row">
                 <div class="col-4 col-md-3 mb-4">
                     <p class="text-white fs-16"><i aria-hidden="true" class="fa fa-asterisk mr-2"></i>Featured Event</p>
@@ -11,42 +11,13 @@
             </div>
             <div class="row">
                 <div class="col-md-7 mb-3">
-                    <h4 class="text-white fs-24 fw-600 mb-3">Understanding the game</h4>
-                    <p class="font-grey fs-18 lh-2">At fermentum, nullam nam scelerisque proin quis ornare in. Id ut ac vitae enim scelerisque ac id. Eget arcu integer massa dictumst amet feugiat integer. Nunc quisque tincidunt velit volutpat. Amet nec dis vestibulum turpis dapibus.</p>
+                    <h4 class="text-white fs-24 fw-600 mb-3">{{ event.title }}</h4>
+                    <p class="font-grey fs-18 lh-2">{{ event.body }}</p>
                 </div>
                 <div class="col-md-12">
                     <div class="button-group d-flex">
-                        <div class="button-blue float-md-left mr-md-4">
-                            <a href="#">Join Event</a>
-                        </div>
-                        <div class="button-outline float-md-left">
-                            <a href="/events">Leaderboard</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </swiper-slide>
-        <swiper-slide>
-            <div class="row">
-                <div class="col-md-2 mb-4">
-                    <p class="text-white fs-16"><i aria-hidden="true" class="fa fa-asterisk mr-2"></i>Featured Event</p>
-                </div>
-                <div class="col-md-2 mb-4">
-                    <p class="text-white fs-16"><i aria-hidden="true" class="fa fa-calendar-o"></i> May 09, 2020</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-7 mb-3">
-                    <h4 class="text-white fs-24 fw-600 mb-3">Understanding the game</h4>
-                    <p class="font-grey fs-18 lh-2">At fermentum, nullam nam scelerisque proin quis ornare in. Id ut ac vitae enim scelerisque ac id. Eget arcu integer massa dictumst amet feugiat integer. Nunc quisque tincidunt velit volutpat. Amet nec dis vestibulum turpis dapibus.</p>
-                </div>
-                <div class="col-md-12">
-                    <div class="button-group d-flex">
-                        <div class="button-blue float-md-left mr-md-4">
-                            <a href="#">Join Event</a>
-                        </div>
-                        <div class="button-outline float-md-left">
-                            <a href="/events">Leaderboard</a>
+                        <div class="toggle-button" @click="registerModal(event.id)">
+                            Register
                         </div>
                     </div>
                 </div>
@@ -57,6 +28,7 @@
 <script>
     import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
     import 'swiper/css/swiper.css'
+    import moment from "moment";
 
     export default {
         name: 'JacEvents',
@@ -65,7 +37,37 @@
             SwiperSlide
         },
         directives: {
-            swiper: directive
+            swiper: {
+
+            }
+        },
+        data() {
+            return {
+                events: [],
+                swiperOptions: {
+                    loop: true,
+                    slidesPerView: 'auto',
+                    autoplay: {
+                        delay: 2500,
+                    }
+                }
+            }
+        },
+        created() {
+            this.$axios
+                .$get(`/events`)
+                .then((res) => {
+                    this.events = res.events;
+                })
+        },
+        methods: {
+            eventTime (value) {
+                return moment.unix(value).format("MMM DD YYYY");
+            },
+            registerModal (eventID) {
+                this.$modal.show('event-register', {eventID: eventID});
+            }
         }
+
     }
 </script>
