eaefbc12148fbb6dbe85f6451f350768078aa3e0
Ethan Smith <esmith164@mail.valenciacollege.edu>
2020-07-10 23:26:09 -0400

    mod hub count

--- components/JacServers.vue
+++ components/JacServers.vue
@@ -40,7 +40,7 @@
                 </div>
                 <div class="col-md-7">
                     <h4 class="fs-22 fw-600 font-white mt-3 mb-0">Simply Pixel</h4>
-                    <p class="fs-16 font-green">{{ SimplyPixelCount }}/100 Playing!</p>
+                    <p class="fs-16 font-green">{{ SimplyPixelCount }}/200 Playing!</p>
                 </div>
                 <div class="col-md-2">
                     <span class="bg-green status w-100 d-block mt-4">On</span>
@@ -55,11 +55,11 @@
                     <img src="~assets/images/blaze.png" width="76px">
                 </div>
                 <div class="col-md-7">
-                    <h4 class="fs-22 fw-600 font-white mt-3 mb-0">Blaze Games</h4>
-                    <p class="fs-16 font-red">Server is restarting!</p>
+                    <h4 class="fs-22 fw-600 font-white mt-3 mb-0">Modded Hub</h4>
+                    <p class="fs-16 font-green">{{ ModHubCount }}/200 Playing!</p>
                 </div>
                 <div class="col-md-2">
-                    <span class="bg-red status w-100 d-block mt-4">OFF</span>
+                    <span class="bg-green status w-100 d-block mt-4">On</span>
                 </div>
             </div>
         </div>
@@ -88,7 +88,7 @@
                     this.HyWarCount = res.HyWar.count;
                     this.SimplyPixelCount = res.SimplyPixel.count;
                     this.AoSCount = res.AoS.count;
-                    this.ModHub = res.ModHub.count;
+                    this.ModHubCount = res.ModHub.count;
                 })
         }
     }
